e2d19494bc47a29264ce289bb40473bd575e6f6a
Trí Minh <vominhtri13@gmail.com>
2025-03-05 00:48:37 +0700

    Make it easy to change status

--- ClassVision.FrontEnd/src/components/attendingClass/attendeeWithNameColumns.tsx
+++ ClassVision.FrontEnd/src/components/attendingClass/attendeeWithNameColumns.tsx
@@ -4,14 +4,52 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel,
 import { MoreHorizontal } from "lucide-react";
 import { Button } from "../ui/button";
 import { Checkbox } from "../ui/checkbox";
-import { AttendeeType, EAttendantStatus } from "../../interfaces/AttendeeTypes";
+import { AttendeeModifyType, AttendeeType, EAttendantStatus, EAttendantStatusToString, StringToEAttendantStatus } from "../../interfaces/AttendeeTypes";
 import { attendeeModifyStore, attendeeDeleteStore } from "../../stores/attendeeStores";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { Combobox, ComboboxData } from "../ui/combobox";
+import { authorizedFetch } from "../../utils/authorizedFetcher";
 
 const store = attendeeModifyStore
 const deleteStore = attendeeDeleteStore
 const handleDeleteClick = (id: string) => {
     deleteStore.id = id
     deleteStore.opened = true
+}
+const eAttendantCBData : ComboboxData[] = [{
+    label: "Present",
+    value: "Present"
+},
+{
+    label: "Absent",
+    value: "Absent"
+},
+{
+    label: "Late",
+    value: "Late"
+},
+{
+    label: "Excused",
+    value: "Excused"
+},
+{
+    label: "Other",
+    value: "Other"
+},
+]
+
+const handleEdit = async (sentData: AttendeeModifyType) => {
+    const url = `/api/Attendee`
+    const resp = await authorizedFetch(url, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(sentData)
+    })
+
+    const data = await resp.json()
+    console.log(data)
 }
 export const attendeeWithNameColumns: ColumnDef<AttendeeType>[] = [
     {
@@ -65,18 +103,19 @@ export const attendeeWithNameColumns: ColumnDef<AttendeeType>[] = [
         header: ({ column }) => columnSortable(column, "Status"),
         cell: ({ row }) => {
             const current = row.getValue("status") as EAttendantStatus
-            switch (current) {
-                case EAttendantStatus.PRESENT:
-                    return "Present";
-                case EAttendantStatus.ABSENT:
-                    return "Absent"
-                case EAttendantStatus.LATE:
-                    return "Late"
-                case EAttendantStatus.EXCUSED:
-                    return "Excused"
-                case EAttendantStatus.OTHER:
-                    return "Other"
-            }
+            console.log(current)
+            return (
+                <Combobox value={EAttendantStatusToString(current)} onValueChange={(value) => {
+                    handleEdit({
+                        id: row.getValue("id"),
+                        courseId: row.getValue("courseId"),
+                        scheduleId: row.getValue("scheduleId"),
+                        studentId: row.getValue("studentId"),
+                        status: StringToEAttendantStatus(value)
+                    })
+                }} data={eAttendantCBData} className={""}
+                />
+            )
         }
     },
     {
--- ClassVision.FrontEnd/src/interfaces/AttendeeTypes.ts
+++ ClassVision.FrontEnd/src/interfaces/AttendeeTypes.ts
@@ -1,12 +1,45 @@
 import { CommonType, ExcludeCommonType } from "./CommonType";
 import { EnrollmentType } from "./EnrollmentTypes";
 
+
 export const enum EAttendantStatus {
     PRESENT = 0,
     ABSENT = 1,
     LATE = 2,
     EXCUSED = 3,
     OTHER = 4
+}
+
+export const EAttendantStatusToString = (value: EAttendantStatus) => {
+    switch (value) {
+        case EAttendantStatus.PRESENT:
+            return "Present";
+        case EAttendantStatus.ABSENT:
+            return "Absent"
+        case EAttendantStatus.LATE:
+            return "Late"
+        case EAttendantStatus.EXCUSED:
+            return "Excused"
+        case EAttendantStatus.OTHER:
+            return "Other"
+    }
+}
+
+export const StringToEAttendantStatus = (value: string) => {
+    switch (value) {
+        case "Present":
+            return EAttendantStatus.PRESENT;
+        case "Absent":
+            return EAttendantStatus.ABSENT
+        case "Late":
+            return EAttendantStatus.LATE
+        case "Excused":
+            return EAttendantStatus.EXCUSED
+        case "Other":
+            return EAttendantStatus.OTHER
+        default:
+            throw new Error("Value not support")
+    }
 }
 
 
