3a87492c3961c8049beeb120701a60223a2a60e7
Trí Minh <vominhtri13@gmail.com>
2025-03-07 21:30:22 +0700

    Add migration

---
+++ ClassVision.Data/Migrations/20250307142731_ChangeRollCallFace.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassVision.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRollCallFace : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RollcallFaces",
+                table: "RollcallFaces");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentId",
+                table: "RollcallFaces",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "RollcallFaces",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RollcallFaces",
+                table: "RollcallFaces",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RollcallFaces_StudentId_ImageId",
+                table: "RollcallFaces",
+                columns: new[] { "StudentId", "ImageId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RollcallFaces",
+                table: "RollcallFaces");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RollcallFaces_StudentId_ImageId",
+                table: "RollcallFaces");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "RollcallFaces");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentId",
+                table: "RollcallFaces",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RollcallFaces",
+                table: "RollcallFaces",
+                columns: new[] { "StudentId", "ImageId" });
+        }
+    }
+}
